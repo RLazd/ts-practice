31d0c90eb727db6c0a74aae6ee23fa600cc319a7
Ramona Lazdina <ramona.lazdina@helmes.com>
2024-01-26 14:41:05 +0200

    ts compiler - deleted func

--- course-starting-project/src/app.ts
+++ course-starting-project/src/app.ts
@@ -48,12 +48,3 @@ In tsconfig.json
             strictBindCallApply-
             
 */
-function sendRequest(data: string, cb: (response: any) => void) {
-  // ... sending a request with "data"
-  return cb({ data: "Hi there!" });
-}
-
-sendRequest("Send this!", (response) => {
-  console.log(response);
-  return true;
-});
